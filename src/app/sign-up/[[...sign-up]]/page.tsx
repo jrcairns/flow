56015c3f30b45c09642048020a260d2f1b0459f1
Justin Cairns <justincairnsuk@gmail.com>
2024-07-18 03:36:27 -0600

    auth page ui

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
     return (
-        <div className="min-h-screen flex bg-muted/30 p-2">
+        <div className="min-h-screen lg:h-screen flex bg-muted/30 p-2">
             <div className="flex-1 relative flex items-center justify-center">
                 <SignUp appearance={{
                     variables: {
@@ -11,7 +11,7 @@ export default function Page() {
                     }
                 }} />
             </div>
-            <div className="flex-1 border rounded-md bg-background flex items-center overflow-hidden">
+            <div className="hidden flex-1 border rounded-md bg-background lg:flex items-center overflow-hidden">
                 <div className="p-12 relative">
                     <img className="h-full w-full object-cover" src="/ui.png" alt="" />
                     <div className="absolute inset-0 bg-gradient-to-t from-background to-transparent"></div>
